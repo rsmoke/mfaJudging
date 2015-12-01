361f5316340f3b13e5897be95fb9be7f8a03a9ad
Rick Smoke <rsmoke@umich.edu>
2015-12-01 12:47:37 -0500

    add link to new config file

--- evaluation.php
+++ evaluation.php
@@ -1,5 +1,5 @@
 <?php
-require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configEnglishContest.php');
+require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configEnglishContestJudging.php');
 require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/basicLib.php');
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
