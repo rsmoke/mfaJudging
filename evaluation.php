67cbe87a8d274bcdc3ef0564274e5b5979050f88
Rick Smoke <rsmoke@umich.edu>
2015-12-01 23:17:32 -0500

    fix link to file source

--- evaluation.php
+++ evaluation.php
@@ -103,7 +103,7 @@ SQL;
         <nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
           <div class="container">
           <div class="navbar-header">
-             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="judging/index.php"><?php echo "$contestTitle";?><span style="color:#00FF80">-Judging</span></a>
+             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="index.php"><?php echo "$contestTitle";?><span style="color:#00FF80">-Judging</span></a>
           </div>
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
@@ -141,7 +141,7 @@ SQL;
             echo "<div style='padding: 0 0 0 40px;'>";
             echo "<strong>Entry Title: </strong><mark>" . $row["title"] . "</mark>  <br />";
 
-            echo '<a href="' . $_SERVER["DOCUMENT_ROOT"] . '/../contestfiles/' . $row['document'] . '">Read</a><br /><br />';
+            echo '<a href="contestfiles/' . $row['document'] . '" target="_blank">Read</a><br /><br />';
 
             echo "<strong>Authors Pen-name:</strong> " . $row["penName"] ."<br />";
 
