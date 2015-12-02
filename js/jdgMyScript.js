47ef7108b538219306cf2772411adcee87c2d134
Rick Smoke <rsmoke@umich.edu>
2015-12-01 22:46:53 -0500

    clean up whitespace

--- js/jdgMyScript.js
+++ js/jdgMyScript.js
@@ -1,8 +1,6 @@
 $( document ).ready(function(){
-
   $('#contest').on('click', '.btn-eval', function (e){
     var entryid = $(this).data('entryid');
     document.location = 'evaluation.php?evid=' + entryid;
   });
-
 });
