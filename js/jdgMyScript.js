bcdbf861486cbcf3597eef4ad978b7829020fbc5
Rick Smoke <rsmoke@umich.edu>
2015-12-03 17:54:39 -0500

    clean whitespace errors

--- js/jdgMyScript.js
+++ js/jdgMyScript.js
@@ -7,3 +7,40 @@ $( document ).ready(function(){
   $('.disabled').toggleClass("btn-info");
 
 });
+
+$(function(){
+  $('select[id^=rank_]').change(function()
+  {
+      // List of ids that are selected in all select elements
+      var selected = new Array();
+
+      // Get a list of the ids that are selected
+      $('[id^=rank_] option:selected').each(function()
+      {
+          selected.push($(this).val());
+      });
+
+      // Walk through every select option and enable if not 
+      // in the list and not already selected
+      $('[id^=rank_] option').each(function()
+      {
+          if (!$(this).is(':selected') && $(this).val() != '')
+          {
+              var shouldDisable = false;
+              for (var i = 0; i < selected.length; i++)
+              {
+                  if (selected[i] == $(this).val())
+                      shouldDisable = true;
+              }
+
+              $(this).css('text-decoration', '');
+              $(this).removeAttr('disabled', 'disabled');
+              if (shouldDisable)
+              {
+                  $(this).css('text-decoration', 'line-through');
+                  $(this).attr('disabled', 'disabled');
+              }
+          }
+      });
+  });
+});
