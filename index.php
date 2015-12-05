5b73d84b824ac0550b0fdac4ad8daf6de31789e2
Rick Smoke <rsmoke@umich.edu>
2015-12-05 15:14:15 -0500

    make all panels collapsed

--- index.php
+++ index.php
@@ -133,12 +133,12 @@ if (!$results) {
       <div class="panel panel-default">
         <div class="panel-heading" role="tab" id="heading<?php echo $count ?>">
           <h6 class="panel-title">
-            <a data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $count ?>" aria-expanded="false" aria-controls="collapse<?php echo $count ?>">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $count ?>" aria-expanded="false" aria-controls="collapse<?php echo $count ?>">
                 <?php echo $instance['name'] . " <br /><small>closed: </small><span style='color:#0080FF'>" . date_format(date_create($instance['date_closed']),"F jS Y \a\\t g:ia") . "</span>" ?>
             </a>
           </h6>
         </div>
-        <div id="collapse<?php echo $count ?>" class="panel-collapse" role="tabpanel" aria-labelledby="heading<?php echo $count ?>">
+        <div id="collapse<?php echo $count ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading<?php echo $count ?>">
           <div class="panel-body">
              <div class="well well-sm">Eligibility: 
                 <?php
